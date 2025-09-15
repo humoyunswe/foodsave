2a7af4f8469846c310a9a11035cc6f15c95aa7ea
Humoyun Akbaraliev <136078810+humoyunswe@users.noreply.github.com>
2025-09-15 22:31:49 +0500

    add map

--- foodsave/settings.py
+++ foodsave/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'foodsave.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': (BASE_DIR / 'dv.sqlite3') if (BASE_DIR / 'dv.sqlite3').exists() else (BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'foodsave',
+        'USER': 'postgres',
+        'PASSWORD': '55hXumoyun',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
